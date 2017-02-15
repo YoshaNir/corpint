f656ff174022aea3895777e9916830d46db38348
Friedrich Lindenberg <friedrich@pudo.org>
2017-02-15 16:09:59 +0100

    Split up neo loaders into smaller transactions.

--- corpint/export.py
+++ corpint/export.py
@@ -15,13 +15,57 @@ def normalise(data):
     return properties
 
 
-def clear_leaf_nodes(tx, label):
-    tx.run("""MATCH ()-[r]->(n:%s)
+def clear_leaf_nodes(graph, label):
+    graph.run("""MATCH ()-[r]->(n:%s)
         WITH n, collect(r) as rr
         WHERE length(rr) <= 1 AND NOT n-->()
         FOREACH (r IN rr | DELETE r)
         DELETE n
     """ % label)
+
+
+def load_entity(project, graph, entity):
+    tx = graph.begin()
+    try:
+        label = entity.pop('type', None) or 'Other'
+        node = Node(label, **normalise(entity))
+        project.log.info("Node [%s]: %s", label, entity['name'])
+        tx.create(node)
+
+        # create "Name" fake nodes
+        fps = set()
+        for name in entity.get('names', []):
+            fp = fingerprints.generate(name)
+            if fp is None:
+                continue
+            fp = fp.replace(' ', '-')
+            if fp in fps:
+                continue
+            fps.add(fp)
+            alias = Node('Name', name=name, fp=fp)
+            tx.merge(alias, 'Name', 'fp')
+            rel = Relationship(node, 'ALIAS', alias)
+            tx.create(rel)
+
+        addrfps = set()
+        for address in entity.get('address'):
+            fp = fingerprints.generate(address)
+            if fp is None:
+                continue
+            fp = fp.replace(' ', '-')
+            if fp in addrfps:
+                continue
+            addrfps.add(fp)
+            loc = Node('Address', name=address, fp=fp)
+            tx.merge(loc, 'Address', 'fp')
+            rel = Relationship(node, 'LOCATION', loc)
+            tx.create(rel)
+
+        tx.commit()
+        return entity['uid'], node
+    except Exception as err:
+        tx.rollback()
+        return None, None
 
 
 def load_to_neo4j(project, neo4j_uri=None):
@@ -31,57 +75,20 @@ def load_to_neo4j(project, neo4j_uri=None):
         return
     project.log.info("Loading graph to Neo4J: %s", neo4j_uri)
     graph = Graph(neo4j_uri)
-    tx = graph.begin()
-    try:
-        tx.run('MATCH (n) DETACH DELETE n')
-        entities = {}
-        for entity in project.iter_merged_entities():
-            label = entity.pop('type', None) or 'Other'
-            node = Node(label, **normalise(entity))
-            project.log.info("Node [%s]: %s", label, entity['name'])
-            tx.create(node)
-            entities[entity['uid']] = node
 
-            # create "Name" fake nodes
-            fps = set()
-            for name in entity.get('names', []):
-                fp = fingerprints.generate(name)
-                if fp is None:
-                    continue
-                fp = fp.replace(' ', '-')
-                if fp in fps:
-                    continue
-                fps.add(fp)
-                alias = Node('Name', name=name, fp=fp)
-                tx.merge(alias, 'Name', 'fp')
-                rel = Relationship(node, 'ALIAS', alias)
-                tx.create(rel)
+    graph.run('MATCH (n) DETACH DELETE n')
+    entities = {}
+    for entity in project.iter_merged_entities():
+        uid, node = load_entity(project, graph, entity)
+        entities[uid] = node
 
-            addrfps = set()
-            for address in entity.get('address'):
-                fp = fingerprints.generate(address)
-                if fp is None:
-                    continue
-                fp = fp.replace(' ', '-')
-                if fp in addrfps:
-                    continue
-                addrfps.add(fp)
-                loc = Node('Address', name=address, fp=fp)
-                tx.merge(loc, 'Address', 'fp')
-                rel = Relationship(node, 'LOCATION', loc)
-                tx.create(rel)
+    for link in project.iter_merged_links():
+        source = entities.get(link.pop('source'))
+        target = entities.get(link.pop('target'))
+        if source is None or target is None:
+            continue
+        rel = Relationship(source, 'LINK', target, **normalise(link))
+        graph.create(rel)
 
-        for link in project.iter_merged_links():
-            source = entities.get(link.pop('source'))
-            target = entities.get(link.pop('target'))
-            if source is None or target is None:
-                continue
-            rel = Relationship(source, 'LINK', target, **normalise(link))
-            tx.create(rel)
-
-        clear_leaf_nodes(tx, 'Name')
-        clear_leaf_nodes(tx, 'Address')
-        tx.commit()
-    except Exception as ex:
-        project.log.exception(ex)
-        tx.rollback()
+    clear_leaf_nodes(graph, 'Name')
+    clear_leaf_nodes(graph, 'Address')
